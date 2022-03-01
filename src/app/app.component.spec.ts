caead06ed3274b6445bea7916ba882622f9dbfa1
jonathan.heindl <jonathan.heindl@brandad-systems.de>
2022-03-01 11:02:59 +0100

    seems like karma works more async

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,64 +1,72 @@
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { FormProviderDirective } from "./form-provider.directive";
 
-import {FormsModule} from "@angular/forms";
-import {By} from "@angular/platform-browser";
-import {AppComponent} from "./app.component";
-import {FormProviderDirective} from "./form-provider.directive";
+
+
 
 describe('FormProvider AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
+	let component: AppComponent;
+	let fixture: ComponentFixture<AppComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FormsModule],
-      declarations: [AppComponent, FormProviderDirective]
-    })
-      .compileComponents();
-  });
+	async function setValue(inputEl: HTMLInputElement, value: string) {
+		inputEl.value = value;
+		inputEl.dispatchEvent(new Event('input'))
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		await fixture.whenStable()
+	}
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [FormsModule],
+			declarations: [AppComponent, FormProviderDirective]
+		})
+			.compileComponents();
+	});
 
-  it('injected directive ohne async', () => {
-    expect(component).toBeTruthy()
-    expect(FormProviderDirective.form.value).toEqual({test: '123'})
-    expect(FormProviderDirective.form.valid).toBeTruthy()
-    const input = fixture.nativeElement.querySelector('input');
-    input.value = 'Red';
-    input.dispatchEvent(new Event('input'))
-    fixture.detectChanges();
-    expect(FormProviderDirective.form.value).toEqual({test: 'Red'})
-    expect(FormProviderDirective.form.valid).toBeTruthy()
-    input.value = '';
-    input.dispatchEvent(new Event('input'))
-    fixture.detectChanges();
-    expect(FormProviderDirective.form.value).toEqual({test: ''})
-    expect(FormProviderDirective.form.valid).toBeFalse()
-  })
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AppComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('injection directive waitForAsync', waitForAsync(() => {
-    fixture.whenStable().then(() => {
-      expect(FormProviderDirective.form.value).toEqual({test: ''})
-      expect(FormProviderDirective.form.valid).toBeFalse()
-      const input = fixture.debugElement.query(By.css("input"));
-      input.nativeElement.value = 'Red';
-      input.nativeElement.dispatchEvent(new Event('input'))
-      fixture.detectChanges();
-      expect(FormProviderDirective.form.value).toEqual({test: 'Red'})
-      expect(FormProviderDirective.form.valid).toBeTruthy()
-      input.nativeElement.value = '';
-      input.nativeElement.dispatchEvent(new Event('input'))
-      fixture.detectChanges();
-      expect(FormProviderDirective.form.value).toEqual({test: ''})
-      expect(FormProviderDirective.form.valid).toBeFalse()
-    })
-  }))
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('injected directive ohne async', async () => {
+		await fixture.whenStable()
+		expect(component).toBeTruthy()
+		expect(FormProviderDirective.form.value).toEqual({ test: '' })
+		expect(FormProviderDirective.form.valid).toBeFalse()
+
+		const input = fixture.nativeElement.querySelector('input');
+		await setValue(input, "Red")
+		expect(FormProviderDirective.form.value).toEqual({ test: 'Red' })
+		expect(FormProviderDirective.form.valid).toBeTruthy()
+
+		await setValue(input, "")
+		expect(FormProviderDirective.form.value).toEqual({ test: '' })
+		expect(FormProviderDirective.form.valid).toBeFalse()
+	})
+
+	it('injection directive waitForAsync', waitForAsync(() => {
+		fixture.whenStable().then(() => {
+			expect(FormProviderDirective.form.value).toEqual({ test: '' })
+			expect(FormProviderDirective.form.valid).toBeFalse()
+			const input = fixture.debugElement.query(By.css("input"));
+			input.nativeElement.value = 'Red';
+			input.nativeElement.dispatchEvent(new Event('input'))
+			fixture.detectChanges();
+			expect(FormProviderDirective.form.value).toEqual({ test: 'Red' })
+			expect(FormProviderDirective.form.valid).toBeTruthy()
+			input.nativeElement.value = '';
+			input.nativeElement.dispatchEvent(new Event('input'))
+			fixture.detectChanges();
+			expect(FormProviderDirective.form.value).toEqual({ test: '' })
+			expect(FormProviderDirective.form.valid).toBeFalse()
+		})
+	}))
 });
